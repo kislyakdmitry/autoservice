4b4a5a48fec5420cb84f6f1d7d35fbc31318467c
kislyakdmitry <kislyakdmitry@gmail.com>
2019-08-30 16:20:19 +0300

    New contract form

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component'
 import { ContractsComponent } from './contracts/contracts.component'
+import { NewContract } from './new_contract/new_contract.component'
 
 
 const routes: Routes = [
-    { path: '', component: ContractsComponent},
-    { path: 'login', component: LoginComponent}
+    { path: 'new', component: NewContract},
+    { path: 'login', component: LoginComponent},
+    { path: '', component: ContractsComponent}
 ];
 
 @NgModule({
---
+++ public/src/app/newContract.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Contract} from './contract';
+import { Observable, throwError } from 'rxjs';
+import { ResourceService } from './resource.service';
+
+@Injectable()
+export class ContractsService{
+    constructor(private http: HttpClient, private resourceService: ResourceService){}
+
+    private contractsUrl = 'http://localhost:8080/api/contracts';
+
+    getData(): Observable<Contract[]> {
+        return this.resourceService.getResource(this.contractsUrl);
+    }
+}
